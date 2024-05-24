c5a8ff09c79607d53b389db72adf890a2a016f4d
8ad40n <joy184110@gmail.com>
2024-05-25 01:22:04 +0600

    Multiset

---
+++ STL/multiset.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void multisetSolve()
+{
+    // same as set but stores duplicate element also
+
+    multiset<int> s;
+    s.insert(1); // 1
+    s.insert(2); // 1 2
+    s.insert(2); // 1 2 2
+
+    int cnt = count(2); // Ans: 2 // will count the number of 2
+    
+}
+
+int main()
+{
+    multisetSolve();
+}

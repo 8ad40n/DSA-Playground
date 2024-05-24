7f4e2bdab838ffb293fa456cd3a1bc084f72e74e
8ad40n <joy184110@gmail.com>
2024-05-24 23:15:04 +0600

    deque

---
+++ STL/deque.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void dequeSolve()
+{
+    deque<int> dq;
+    dq.push_back(10);
+    dq.push_front(20);
+
+    dq.back();
+    dq.front();
+
+    // rest functions are same as vector
+
+}
+
+int main()
+{
+    dequeSolve();
+}

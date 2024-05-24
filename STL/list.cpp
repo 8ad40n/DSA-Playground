bb1630f67c0467d3f844a1ac4d35b8da15052390
8ad40n <joy184110@gmail.com>
2024-05-24 23:11:14 +0600

    List added

---
+++ STL/list.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void listSolve()
+{
+    list<int> ls;
+    ls.push_back(2);
+    ls.emplace_back(3);
+
+
+    // insert in vector is costly.
+    ls.push_front(10);
+    ls.emplace_front(20);
+
+    // rest functions are same as vector   
+}
+
+int main()
+{
+    listSolve();
+}

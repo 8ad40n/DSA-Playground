07fb5ecf87b4f886326019e8989abc0128afe0b3
8ad40n <joy184110@gmail.com>
2024-06-02 21:39:50 +0600

    Optimal move 0 at end

--- Array/Easy/moveAllZeroAtEnd.cpp
+++ Array/Easy/moveAllZeroAtEnd.cpp
@@ -7,7 +7,7 @@ using namespace std;
 void moveAllZeroAtEnd(int arr[], int n)
 {
     // O(2n)
-    
+
     vector<int>temp;
     // non zeros storing in temp
     for(int i=0;i<n;i++)
@@ -36,11 +36,51 @@ void moveAllZeroAtEnd(int arr[], int n)
 
 }
 
+void moveAllZeroAtEndOptimal(int arr[], int n)
+{
+    // O(n)
+
+    int j= INT_MIN;
+    
+    // storing index of 0 in j
+    for(int i=0;i<n;i++)
+    {
+        if(arr[i]==0)
+        {
+            j=i;
+            break;
+        }
+    }
+
+    // swap i with i+1 
+    for(int i=j+1;i<n;i++)
+    {
+        if(arr[i] != 0)
+        {
+            swap(arr[i], arr[j]);
+            j++;
+        }
+    }
+
+    // print
+    for (int i = 0; i < n; i++)
+    {
+        cout<<arr[i]<<" ";
+    }
+    
+    
+}
+
+
 
 int main()
 {
     int n=7;
     int arr[7]={2,0,1,3,0,5,0};
     moveAllZeroAtEnd(arr,n);
+
+    cout<<endl;
+    int arr1[7]={2,0,1,3,0,5,0};
+    moveAllZeroAtEndOptimal(arr1,n);
 
 }

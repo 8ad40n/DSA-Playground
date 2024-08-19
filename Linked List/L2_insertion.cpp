a2ce60e4fb532f953876691676ff8b93d8d11417
8ad40n <joy184110@gmail.com>
2024-08-19 19:46:27 +0600

    insert at head

---
+++ Linked List/L2_insertion.cpp
@@ -0,0 +1,149 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+struct Node{
+    public: 
+    int data; 
+    Node* next; 
+
+    public:
+    Node (int data1, Node* next1){
+        data=data1;  
+        next=next1;  
+    }
+    Node (int data1){
+        data=data1;  
+        next=nullptr;
+    }
+};
+
+// convert to ll
+Node* convertToLL(vector<int>&arr)
+{
+    Node* head = new Node (arr[0]);
+    Node* mover = head;
+
+    for(int i =1;i<arr.size();i++)
+    {
+        Node* temp = new Node(arr[i]);
+        mover->next = temp;
+        mover = temp;
+    }
+    return head;
+}
+
+// print
+Node* print(Node* head)
+{
+    Node* temp = head;
+    while(temp)
+    {
+        cout<< temp->data<<" ";
+        temp = temp->next;
+    }
+    cout<<endl;
+}
+
+// Insert at head
+Node* InsertHead(Node* head, int val)
+{
+    Node* temp = new Node(val,head);
+    return temp;
+}
+// Delete tail
+Node* DeleteTail(Node* head)
+{
+    if (head == NULL || head->next == NULL) return NULL;
+
+    Node* temp = head;
+    while (temp->next->next != NULL) 
+    {
+        temp = temp->next;
+    }
+    delete temp->next;
+   
+    temp->next = nullptr;
+    return head;
+}
+
+// Delete from position
+Node* DeleteFromPosition(Node* head, int k)
+{
+    if(head == NULL) return head;
+    if(k==1)
+    {
+       Node* temp = head;
+       head = head->next;
+       delete  temp;
+       return head; 
+    }
+
+    int cnt = 0;
+    Node* temp = head;
+    Node* prev = NULL;
+
+    while (temp!= NULL)
+    {
+        cnt++;
+        if(cnt == k)
+        {
+            prev->next = prev->next->next;
+            delete temp;
+            break;
+        }
+        prev = temp;
+        temp = temp->next;
+    }
+    return head;
+}
+
+
+// Delete with value
+Node* DeleteWithValue(Node* head, int val)
+{
+    if(head == NULL) return head;
+    if(head->data == val)
+    {
+       Node* temp = head;
+       head = head->next;
+       delete  temp;
+       return head; 
+    }
+
+    Node* temp = head;
+    Node* prev = NULL;
+
+    while (temp!= NULL)
+    {
+        if(temp->data == val)
+        {
+            prev->next = prev->next->next;
+            delete temp;
+            break;
+        }
+        prev = temp;
+        temp = temp->next;
+    }
+    return head;
+}
+
+int main()
+{
+    vector<int> arr={2,5,8,7};
+    Node* head = convertToLL(arr);
+    cout<<head->data<<endl;
+    print(head);
+
+    Node* insertHead = InsertHead(head, 100);
+    print(insertHead);
+
+    // Node* deleteTail = DeleteTail(head);
+    // print(deleteTail);
+
+    // Node* deleteFromPosition = DeleteFromPosition(head,3);
+    // print(deleteFromPosition);
+
+    // Node* deleteWithValue = DeleteWithValue(head,5);
+    // print(deleteWithValue);
+
+}
